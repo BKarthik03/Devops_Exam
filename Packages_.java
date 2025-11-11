e3359d9ec96c0e6d952adee12294e562288932bc
Karthik RS <106664784+BKarthik03@users.noreply.github.com>
2025-11-11 17:40:46 +0530

    Add Packages_ class to sort and print an array

---
+++ Packages_.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Packages_
+{
+  public static void main(String args[])
+  {
+    int arr[] = {1,5,6,8,2,4,3,9};
+    Arrays.sort(arr);
+    for(int i=0;i<arr.length;i++)
+    {
+      System.out.println(arr[i]);
+    }
+  }
+}
